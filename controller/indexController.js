4ed30a6511a50b9a433fb4d4ab33bcb8d18c1e52
mosquerapaul <mosquera.paul@gmail.com>
2019-05-28 09:58:51 +0200

    playlistController adds mp3 files filter

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,11 +1,10 @@
 var fs = require('fs');
 
-const indexController = (req, res) => {
+const indexController = (req, res, next) => {
     fs.readFile('index.html', 'utf8', (err, data) => {
         if (err) throw err;
         res.send(data);
     });
-
 }
 
 module = module.exports = {indexController};
--- controller/playlistController.js
+++ controller/playlistController.js
@@ -4,16 +4,15 @@ const AUDIOFOLDER = './audio';
 
 const playlistController = (req, res) => {
     var audioList;
-    fileList = new Promise( (resolve, reject) => {
-        fs.readdir(AUDIOFOLDER, (err, files) => {
-            console.log(files);
-            audioList = files.reduce((ac, audio, ind, arr) => {
-                ac += '<li class="audio">' + audio + '</li>';
-                return ac;
-            }, '<ol>');
-            audioList += '</ol>';
-            res.send(audioList);
-        });
+    fs.readdir(AUDIOFOLDER, (err, files) => {
+        files = files.filter(file => /\.mp3$/.test(file.toLocaleLowerCase()))
+        console.log(files);
+        audioList = files.reduce((ac, audio, ind, arr) => {
+            ac += '<li class="audio">' + audio + '</li>';
+            return ac;
+        }, '<ol>');
+        audioList += '</ol>';
+        res.send(audioList);
     });
 }
 
